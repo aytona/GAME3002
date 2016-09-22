2a4839c3e397ffca3c4a4dafaf78617ab43b35f8
Christopher Aytona <christopher.aytona@gmail.com>
2016-09-21 23:09:48 -0400

    lab 3

---
+++ Lab3/Impulse.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Impulse : MonoBehaviour {
+
+	public float distance;
+	public float time;
+	public float mass;
+	public float finalVelocity;
+
+	private float initialVelocity;
+	private float acceleration;
+	private float force;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.mass = mass;
+		initialVelocity = ((distance / time) * 2) - finalVelocity;
+		acceleration = ((Mathf.Pow(initialVelocity, 2) - Mathf.Pow(finalVelocity, 2))
+						/ distance) / 2;
+		force = (mass * (initialVelocity - finalVelocity)) / Time.deltaTime;
+		rb.AddForce(Vector3.up  * force);
+	}
+}
---
+++ Lab3/Lab3Projectile.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Lab3Projectile : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float m_displacement;
+	public float m_initialVelocity;
+	public float m_finalVelocity;
+	public float m_acceleration;
+	public float m_time;
+	public Vector3 m_forceDirection = Vector3.up;
+
+	private Vector3 m_Impulse;
+	public bool m_isFlappyBird = false;
+
+	private float CalcInitVelocity(float fVelocity, float displacement, float time)
+	{
+		return time > Mathf.Epsilon ? (displacement / time) * 2.0f - fVelocity : 0.0f;
+	}
+
+	private float CalcImpulse(float iV, float fV, float mass)
+	{
+		return ((fV - iV) * mass) / Time.deltaTime; 
+	}
+
+	void Start()
+	{
+		m_initialVelocity = CalcInitVelocity(m_finalVelocity, m_displacement, m_time);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			 m_isFlappyBird = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		m_Impulse = m_forceDirection * CalcImpulse(m_initialVelocity, m_finalVelocity, rb.mass);
+
+		if (m_isFlappyBird)
+		{
+			rb.AddForce(m_Impulse);
+			m_isFlappyBird = false;
+		}
+	}
+}

1e09c3ef079218fe74f27efe10bf439fe67a8f93
Christopher Aytona <christopher.aytona@gmail.com>
2016-09-28 23:07:03 -0400

    Lab 4 init

---
+++ Lab4/Assets/Lab4.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lab4 : MonoBehaviour {
+
+	public float m_distance;
+	public float m_height;
+	public float m_time;
+
+	private float m_hypotenuse;
+	private float m_velocity;
+	private float m_angle;
+	private float m_gravity = -9.8f;
+
+	private float GetHypotenuse(float x, float y)
+	{
+		return Mathf.Sqrt((x * x) + (y * y));
+	}
+
+	void Start ()
+	{
+		m_hypotenuse = GetHypotenuse (m_distance, m_height);
+		//20 = m_velocity * 5 + 0.5(-9.8)(5^2);
+		m_velocity = (m_time + (0.5 * m_gravity * (m_time * m_time))) / m_distance;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
